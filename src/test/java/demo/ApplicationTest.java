9169adb3f1a47f385f2740c76bfd12286e9840ac
Caleb Cushing <xenoterracide@gmail.com>
2014-11-11 22:34:11 -0600

    add test to find by uuid

--- src/test/java/demo/ApplicationTest.java
+++ src/test/java/demo/ApplicationTest.java
@@ -4,9 +4,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.data.domain.Persistable;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.UUID;
+
 import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -21,8 +25,11 @@ public class ApplicationTest {
 
         Character c = new Character();
 
-        assertThat(repository.save(c), instanceOf(Character.class));
+        Persistable<UUID> saved = repository.save( c );
+        assertThat( saved, instanceOf( Character.class ) );
 
+        Persistable<UUID> loaded = repository.findOne( saved.getId() );
+        assertEquals( loaded, saved );
     }
 
 }
