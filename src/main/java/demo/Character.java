2f4ad92b1c698fe7a5446d4cae04c49164f02427
Caleb Cushing <xenoterracide@gmail.com>
2014-11-11 23:27:42 -0600

    remove problematic noise annotations

--- src/main/java/demo/Character.java
+++ src/main/java/demo/Character.java
@@ -1,11 +1,9 @@
 package demo;
 
 import org.hibernate.annotations.GenericGenerator;
-import org.hibernate.annotations.Type;
 import org.springframework.data.jpa.domain.AbstractPersistable;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotNull;
 import java.util.UUID;
 
 @Entity
@@ -15,9 +13,7 @@ public class Character extends AbstractPersistable<UUID> {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @NotNull
     @Override
-    @Type(type = "pg-uuid")
     @GeneratedValue(generator = "uuid2")
     @GenericGenerator(name = "uuid2", strategy = "uuid2")
     @Column(name = "task_id", columnDefinition = "uuid")
